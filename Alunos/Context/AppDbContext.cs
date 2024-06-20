e464c02ab2e77fc555fb32e8ccb1a1dd24eb7a47
Aguinaldo <guiga_oli@hotmail.com>
2024-06-20 17:14:15 -0300

    Add project files.

---
+++ Alunos/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Alunos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alunos.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
