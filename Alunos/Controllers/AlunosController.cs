7c969e8d61a61d92d102ceda2ec2d8387051735b
Aguinaldo <guiga_oli@hotmail.com>
2024-06-21 17:11:22 -0300

    feat: implementing get method in student controller

--- Alunos/Controllers/AlunosController.cs
+++ Alunos/Controllers/AlunosController.cs
@@ -1,4 +1,5 @@
-﻿using Alunos.Services;
+﻿using Alunos.Models;
+using Alunos.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,13 +7,31 @@ namespace Alunos.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
+    //Retorna os dados em formato json
+
+    //A classe herda do ControllerBase que é do framework
     public class AlunosController : ControllerBase
     {
         private IAlunoService _alunoService;
 
         public AlunosController(IAlunoService alunoService)
         {
+            //_alunoService salva o contexto dos métodos da interface
             _alunoService = alunoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos()
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunos();
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error ao obter alunos");
+            }
         }
     }
 }
