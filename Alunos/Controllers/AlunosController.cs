e0c91ed1666a34fc48f6a401c2f39b4017183f51
Aguinaldo <guiga_oli@hotmail.com>
2024-06-25 14:00:26 -0300

    feat: carried out the entire implementation of the application controller

--- Alunos/Controllers/AlunosController.cs
+++ Alunos/Controllers/AlunosController.cs
@@ -7,7 +7,7 @@ namespace Alunos.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Produces("application/json")]
+    //[Produces("application/json")]
     //Retorna os dados em formato json
 
     //A classe herda do ControllerBase que é do framework
@@ -27,11 +27,101 @@ namespace Alunos.Controllers
             try
             {
                 var alunos = await _alunoService.GetAlunos();
+                return Ok(alunos);
             }
             catch
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "Error ao obter alunos");
+
             }
+        }
+
+        [HttpGet("AlunosPorNome")]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunosByNome([FromQuery] string nome)
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunosByNome(nome);
+                if (alunos == null)
+                    return NotFound($"Não existe aluno com esse nome {nome}");
+                return Ok(alunos);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error ao obter alunos");
+
+            }
+
+        }
+        [HttpGet("{id:int}", Name = "GetAluno")]
+        public async Task<ActionResult<Aluno>> GetAluno(int id)
+        {
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+                if (aluno == null)
+                    return NotFound($"não existe aluno com essa essa id:{id}");
+                return Ok(aluno);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error ao obter aluno");
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(Aluno aluno)
+        {
+            try
+            {
+                await _alunoService.CreateAluno(aluno);
+                return CreatedAtRoute(nameof(GetAluno), new { id = aluno.Id }, aluno);
+            }
+            catch
+            {
+                return BadRequest("Request inválido");
+            }
+
+        }
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Editar(int id,[FromBody]Aluno aluno)
+        {
+
+            try
+            {
+                if (aluno.Id == id )
+                {
+                    await _alunoService.UpdateAluno(aluno);
+                    return Ok($"aluno com id={id} foi atualizado com sucesso");
+                }
+                else
+                {
+                    return BadRequest("Dados errados");
+                }
+            }
+            catch
+            {
+                return BadRequest("Request inválido");
+            }
+
+
+        }
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int id)
+        {
+
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+                if (aluno != null)
+                 await _alunoService.DeleteAluno(aluno);
+                return Ok($"Aluno de id:{id} foi deletado com sucesso");
+            }
+            catch
+            {
+                return BadRequest("Request inválido");
+            }
+
+
         }
     }
 }
